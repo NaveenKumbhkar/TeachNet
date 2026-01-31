4da28d6b5d2914fd00d2b78446d6dc2983834520
NaveenKumbhkar <144580112+NaveenKumbhkar@users.noreply.github.com>
2026-01-31 17:23:48 +0530

    Add padding to ForgotPassword component

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -16,7 +16,7 @@ const ForgotPassword = () => {
         dispatch(getPasswordResetToken(email, setEmailSend));
     }
     return(
-        <div className="min-h-[calc(100vh-3.5rem)] flex justify-center items-center">
+        <div className="min-h-[calc(100vh-3.5rem)] flex justify-center items-center px-4">
             {
                 loading ? (<div className="spinner"></div>)
                 : (
