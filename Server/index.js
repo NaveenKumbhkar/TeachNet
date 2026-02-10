94357aadaa6b8e09a82279b0777721f9532875d9
Naveen kumbhkar <naveenkumbhkar241@gmail.com>
2026-02-10 06:59:08 +0530

    update index.js

--- Server/index.js
+++ Server/index.js
@@ -58,6 +58,19 @@ app.get("/",(req,res) => {
     });
 });
 
+
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "../dist/index.html")
+  );
+});
+
+
+
 app.listen(PORT,() => {
     console.log(`App is running at ${PORT}`);
 });
