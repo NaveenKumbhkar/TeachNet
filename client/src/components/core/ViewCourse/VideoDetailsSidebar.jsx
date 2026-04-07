79fb01af8489cf80cb25f7c7d120e300fecab46f
NaveenKumbhkar <144580112+NaveenKumbhkar@users.noreply.github.com>
2026-04-08 01:34:14 +0530

    Update VideoDetailsSidebar.jsx

--- client/src/components/core/ViewCourse/VideoDetailsSidebar.jsx
+++ client/src/components/core/ViewCourse/VideoDetailsSidebar.jsx
@@ -424,7 +424,6 @@ export default function VideoDetailsSidebar({ setReviewModal }) {
 
       {/* ================= DESKTOP VIEW ================= */}
       <div className="hidden fixed lg:flex h-[calc(100vh-3.5rem)] w-[320px] max-w-[350px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800">
-      <div className="hidden lg:flex h-[calc(100vh-3.5rem)] w-[320px] max-w-[350px] flex-col border-r-[1px] border-r-richblack-700 bg-richblack-800">
         <div className="mx-5 flex flex-col items-start justify-between gap-2 gap-y-4 border-b border-richblack-600 py-5 text-lg font-bold text-richblack-25">
            <div className="flex w-full items-center justify-between ">
              <div
